0d3bf01b3481923a71ce8a4cbd2db915720a5e3b
sobti <42212648+sobti@users.noreply.github.com>
2024-12-01 01:23:52 +0000

    Update model.py

--- model.py
+++ model.py
@@ -6,20 +6,33 @@ import torchvision
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 class SimpleCNN(nn.Module):
-    def __init__(self):
+     def __init__(self):
         super(SimpleCNN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(32 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 10)
-        
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, padding=1),
+            nn.BatchNorm2d(8),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        self.fc1 = nn.Linear(in_features=16*6*6, out_features=10)
+   
+
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, 32 * 7 * 7)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+
+        return out
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
