fd808a1a034e75295aff2214d38d62ac12a926e7
punit.sobti <punit.sobti@gmail.com>
2024-12-01 00:58:39 +0000

    Initial version

---
+++ model.py
@@ -0,0 +1,24 @@
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(32 * 7 * 7, 128)
+        self.fc2 = nn.Linear(128, 10)
+        
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, 32 * 7 * 7)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def save_model(model, accuracy):
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    filename = f'model_acc{accuracy:.2f}_{timestamp}.pth'
+    torch.save(model.state_dict(), filename)
+    return filename
