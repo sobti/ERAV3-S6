99b8fcc5cc7e47a50a1c9405890435169c9f91d2
sobti <42212648+sobti@users.noreply.github.com>
2024-12-01 01:16:25 +0000

    Update model.py

--- model.py
+++ model.py
@@ -1,3 +1,10 @@
+# Import necessary libraries
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 class SimpleCNN(nn.Module):
     def __init__(self):
         super(SimpleCNN, self).__init__()
