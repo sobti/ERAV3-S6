191853af2a5ad1cd316b565b1320912f3c298f28
punit.sobti <punit.sobti@gmail.com>
2024-12-20 23:46:49 +0000

    Initial

--- train.py
+++ train.py
@@ -43,8 +43,8 @@ def train_model():
         transforms.Normalize((0.1307,), (0.3081,))
     ])
     
-    train_dataset = datasets.MNIST('./data', train=True, download=True, transform=train_transform)
-    test_dataset = datasets.MNIST('./data', train=False, transform=test_transform)
+    train_dataset = datasets.MNIST('./data', train=True, download=True, transform=train_transforms)
+    test_dataset = datasets.MNIST('./data', train=False, transform=test_transforms)
     
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1000)
