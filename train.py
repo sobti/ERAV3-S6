af0484d5cf20fc9c1ad82111ecac0deafe890c4f
punit.sobti <punit.sobti@gmail.com>
2024-12-21 00:27:50 +0000

    Initial

--- train.py
+++ train.py
@@ -132,7 +132,7 @@ def test(model, device, test_loader):
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
-    
+    accuracy = 100 * correct / len(test_loader.dataset)
     test_acc.append(100. * correct / len(test_loader.dataset))
     
     # Save model with timestamp
